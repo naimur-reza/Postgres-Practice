7d5d4eb26aea01f94fa46fa547cf2c38ad9350e6
Naimur Reza <naimur.rezaa@gmail.com>
2024-03-02 17:54:40 +0600

    Add ON DELETE CASCADE

--- foreignKey.sql
+++ foreignKey.sql
@@ -5,12 +5,34 @@ CREATE TABLE "user" (
     id SERIAL PRIMARY KEY, user_name VARCHAR(25) NOT NULL
 );
 
+DROP TABLE post;
+
+DROP Table "user"
+
+ALTER TABLE "user"
+DROP COLUMN email
 -- THAT MAKE POST TABLE MAKE RELATION WITH USER TABLE
 
 CREATE TABLE post (
-    id SERIAL PRIMARY KEY, title VARCHAR(100) NOT NULL, user_id INTEGER REFERENCES "user" (id)
+    id SERIAL PRIMARY KEY, title VARCHAR(100) NOT NULL, user_id INTEGER REFERENCES "user" (id) on delete cascade
 )
 
-INSERT INTO "user" (user_name) VALUES ('John');
+INSERT INTO
+    "user" (user_name)
+VALUES ('Abdul'),
+    ('Rahman'),
+    ('Rahim'),
+    ('Karim');
+
+INSERT INTO
+    post (title, user_id)
+VALUES ('first post', 1),
+    ('second post', 2),
+    ('third post', 3),
+    ('fourth post', 4);
 
 SELECT * FROM "user";
+
+SELECT * FROM post
+
+DELETE FROM "user" WHERE id = 4;
